0f4759c5cbe193da05568de86faa4e76b6e2b5e8
Alexandru Sabau <alexandru.sabau@moduscreate.com>
2016-11-17 11:59:28 +0200

    Added automation test environment

---
+++ test/e2e/wdio.conf.js
@@ -0,0 +1,58 @@
+exports.config = {
+
+    /**
+     * server configurations
+     */
+    maxInstances: 1,
+    host: '0.0.0.0',
+    port: 4444,
+
+    /**
+     * specify test files
+     */
+    specs: [
+        './test/e2e/specs/*.spec.js'
+    ],
+    exclude: [
+        'test/spec/multibrowser/**',
+        'test/spec/mobile/**'
+    ],
+
+    /**
+     * capabilities
+     */
+     capabilities: [{
+       browserName: 'chrome'
+     }],
+
+    /**
+     * test configurations
+     */
+    logLevel: 'silent',
+    sync: true,
+    coloredLogs: true,
+    screenshotPath: './tests/screenshots',
+    baseUrl: 'http://localhost:3000',
+    waitforTimeout: 10000,
+    framework: 'jasmine',
+
+    reporters: ['spec'],
+    reporterOptions: {
+    outputDir: './'
+    },
+
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 9999999
+    },
+
+    /**
+     * hooks
+     */
+    onPrepare: function() {
+        console.log('Starting end2end tests');
+    },
+    onComplete: function() {
+        console.log('All done!');
+    }
+
+};
